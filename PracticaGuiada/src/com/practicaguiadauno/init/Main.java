6ed6aeec1cdb19c8bae5e8c8ca60e6b8fdc17b02
Dreik <derekfn27@gmail.com>
2026-04-29 09:57:09 -0600

    Borré viejo controller

--- PracticaGuiada/src/com/practicaguiadauno/init/Main.java
+++ PracticaGuiada/src/com/practicaguiadauno/init/Main.java
@@ -1,17 +1,13 @@
 package com.practicaguiadauno.init;
 
 import com.formdev.flatlaf.intellijthemes.FlatGradiantoNatureGreenIJTheme;
-import com.practicaguiadauno.mvc.controllers.Controller;
 import com.practicaguiadauno.mvc.controllers.MainController;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int n=0;
-		int y=10;
 		FlatGradiantoNatureGreenIJTheme.setup();
 		new MainController();
 	}
-// HOLA RAMSEY
 	
 }
